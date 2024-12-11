203eee363b1e3f5da1435ba2584995d22f60bf40
phucwin26 <pdnguyen112@student.hbuhsd.edu>
2024-12-11 21:49:18 +0000

    almost done

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,14 @@
 
 int main()
 {
-  // write code here
-  
+  //SET age TO 16
+  int age = 16;
+  double price = 19.99;
+  bool hasPets = false;
+
+  std::cout << age << "\n" << price << std::endl << hasPets << std::endl;
+
+  std::cout << "I am " << age << " years old.\n";
+
   return 0;
 }
